ffc58d9e4ccd85759770fcd3b423858a747073ec
nakani <diogo.guedes.pereira@gmail.com>
2019-09-10 23:11:14 -0300

    ´ajuste´

--- tools/distServer.js
+++ tools/distServer.js
@@ -11,10 +11,7 @@ console.log(chalkProcessing('Opening production build...'));
 
 // Run Browsersync
 browserSync({
-  port: 4000,
-  ui: {
-    port: 4001
-  },
+
   server: {
     baseDir: 'dist'
   },
