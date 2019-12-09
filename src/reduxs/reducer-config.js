12e8f8062a627536cbf1a20ed778ec21a2034c2d
olaviolacerda <olavio.lacerda@hotmail.com>
2019-12-08 23:45:08 -0300

    added redux configs for users

--- src/reduxs/reducer-config.js
+++ src/reduxs/reducer-config.js
@@ -2,12 +2,13 @@ import { combineReducers } from 'redux'
 import productsReducer from './products/product.reducer'
 import imeisReducer from './products/imei.reducer'
 import authReducer from './auth/auth.reducer'
+import usersReducer from './users/user.reducer'
 
 const rootReducer = combineReducers({
   productsReducer,
   imeisReducer,
-  authReducer
-
+  authReducer,
+  usersReducer
 })
 
 export default rootReducer
---
+++ src/reduxs/users/user.action.js
@@ -0,0 +1,13 @@
+import { database } from '../../services/database'
+
+export const getUsersList = async (dispatch) => {
+    dispatch({ type: 'FETCH_USERS_REQUEST', payload: true })
+
+    const users = await database.getUsers() || {}
+
+    if (users) {
+        dispatch({ type: 'FETCH_USERS_SUCCESS', payload: { users } })
+    } else {
+        dispatch({ type: 'FETCH_USERS_FAIL', payload: false })
+    }
+}
