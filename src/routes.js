6982265382ac469b9de88838ad09348899df9faa
olaviolacerda <olavio.lacerda@hotmail.com>
2019-12-27 23:55:10 -0300

    refactored presentation name 'Usuários' to 'Clientes'

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
-import People from '@material-ui/icons/People';
+import People from "@material-ui/icons/People";
 
 // core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.js";
@@ -8,7 +8,6 @@ import UserProfile from "views/UserProfile/UserProfile.js";
 import TableList from "views/TableList/TableList.js";
 import UsersList from "views/UsersList/UsersList.js";
 import TableListImeis from "views/TableList/TableListImeis.js";
-
 
 const dashboardRoutes = [
   {
@@ -45,13 +44,12 @@ const dashboardRoutes = [
   },
   {
     path: "/sellers",
-    name: "Lista De Usuários",
+    name: "Lista De Clientes",
     icon: People,
     component: UsersList,
     layout: "/admin",
     menu: true
   }
-
 ];
 
 const childrenRoutes = [
@@ -60,9 +58,8 @@ const childrenRoutes = [
     name: "Imeis",
     component: TableListImeis,
     layout: "/admin"
-  },]
-
-
+  }
+];
 
 export const routes = {
   dashboardRoutes,
