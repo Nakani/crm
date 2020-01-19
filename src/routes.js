18d9cf05432c52c9990359cb76f4d4c4a06c1272
olaviolacerda <olavio.lacerda@hotmail.com>
2020-01-19 18:34:11 -0300

    added list of sellers with imeis quantity

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import Dashboard from "@material-ui/icons/Dashboard";
-import { Person, People, MonetizationOn } from "@material-ui/icons";
+import { Person, People, MonetizationOn, Receipt } from "@material-ui/icons";
 
 // core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.js";
@@ -8,68 +8,77 @@ import TableList from "views/TableList/TableList.js";
 import UsersList from "views/UsersList/UsersList.js";
 import TableListImeis from "views/TableList/TableListImeis.js";
 import SellsList from "views/SellsList/SellsList.js";
+import SellersList from 'views/SellersList/SellersList.js';
 
 const dashboardRoutes = [
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    icon: Dashboard,
-    component: DashboardPage,
-    layout: "/admin",
-    menu: true
-  },
-  {
-    path: "/user",
-    name: "Financeiro",
-    icon: Person,
-    component: UserProfile,
-    layout: "/admin",
-    menu: true
-  },
-  {
-    path: "/table",
-    name: " Lista De Produtos",
-    icon: "content_paste",
-    component: TableList,
-    layout: "/admin",
-    menu: true
-  },
-  {
-    path: "/imeis",
-    name: "Lista De Imeis",
-    icon: "content_paste",
-    component: TableListImeis,
-    layout: "/admin",
-    menu: false
-  },
-  {
-    path: "/clients",
-    name: "Lista De Clientes",
-    icon: People,
-    component: UsersList,
-    layout: "/admin",
-    menu: true
-  },
-  {
-    path: "/sells",
-    name: "Financeiro",
-    icon: MonetizationOn,
-    component: SellsList,
-    layout: "/admin",
-    menu: true
-  }
+    {
+        path: "/dashboard",
+        name: "Dashboard",
+        icon: Dashboard,
+        component: DashboardPage,
+        layout: "/admin",
+        menu: true
+    },
+    {
+        path: "/user",
+        name: "Perfil",
+        icon: Person,
+        component: UserProfile,
+        layout: "/admin",
+        menu: true
+    },
+    {
+        path: "/table",
+        name: " Lista De Produtos",
+        icon: "content_paste",
+        component: TableList,
+        layout: "/admin",
+        menu: true
+    },
+    {
+        path: "/imeis",
+        name: "Lista De Imeis",
+        icon: "content_paste",
+        component: TableListImeis,
+        layout: "/admin",
+        menu: false
+    },
+    {
+        path: "/clients",
+        name: "Lista De Clientes",
+        icon: People,
+        component: UsersList,
+        layout: "/admin",
+        menu: true
+    },
+    {
+        path: "/sells",
+        name: "Financeiro",
+        icon: MonetizationOn,
+        component: SellsList,
+        layout: "/admin",
+        menu: true
+    },
+    {
+        path: "/sellers",
+        name: "Vendas",
+        icon: Receipt,
+        component: SellersList,
+        layout: "/admin",
+        menu: true
+    }
 ];
 
 const childrenRoutes = [
-  {
-    path: "/table/imeis/:upc",
-    name: "Imeis",
-    component: TableListImeis,
-    layout: "/admin"
-  }
+    {
+        path: "/table/imeis/:upc",
+        name: "Imeis",
+        component: TableListImeis,
+        layout: "/admin"
+    }
 ];
 
 export const routes = {
-  dashboardRoutes,
-  childrenRoutes
+    dashboardRoutes,
+    childrenRoutes
 };
---
+++ src/views/SellersList/SellersList.style.js
@@ -0,0 +1,56 @@
+import { makeStyles } from "@material-ui/core/styles";
+import { grayColor } from "assets/jss/material-dashboard-react.js";
+
+const styles = {
+  cardCategoryWhite: {
+    "&,& a,& a:hover,& a:focus": {
+      color: "rgba(255,255,255,.62)",
+      margin: "0",
+      fontSize: "14px",
+      marginTop: "0",
+      marginBottom: "0"
+    },
+    "& a,& a:hover,& a:focus": {
+      color: "#FFFFFF"
+    }
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none",
+    "& small": {
+      color: "#777",
+      fontSize: "65%",
+      fontWeight: "400",
+      lineHeight: "1"
+    }
+  },
+  cardCategory: {
+    color: grayColor[0],
+    margin: "0",
+    fontSize: "14px",
+    marginTop: "0",
+    paddingTop: "10px",
+    marginBottom: "0"
+  },
+  cardTitle: {
+    color: grayColor[2],
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none",
+    "& small": {
+      color: grayColor[1],
+      fontWeight: "400",
+      lineHeight: "1"
+    }
+  }
+};
+
+export const Styles = makeStyles(styles);
